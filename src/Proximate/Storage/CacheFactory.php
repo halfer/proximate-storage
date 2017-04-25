d63a77b46f1042ec39179575565483525b237b70
Jon Hinks <projects.ws@jondh.me.uk>
2017-04-25 11:01:05 +0100

    Move factory method to its own class

---
+++ src/Proximate/Storage/CacheFactory.php
@@ -0,0 +1,87 @@
+<?php
+
+/**
+ * A factory to create class instances to support a file cache
+ */
+
+namespace Proximate\Storage;
+
+use League\Flysystem\Adapter\Local as LocalFileAdapter;
+use League\Flysystem\Filesystem as FlyFilesystem;
+use Cache\Adapter\Filesystem\FilesystemCachePool;
+use Proximate\Storage\Filesystem as FilesystemCacheAdapter;
+use Proximate\Exception\Init as InitException;
+
+class FilecacheFactory
+{
+    protected $cachePath;
+    protected $cachePool;
+    protected $cacheAdapter;
+
+    public function __construct($cachePath)
+    {
+        $this->cachePath = $cachePath;
+    }
+
+    public function init()
+    {
+        // Get the parent dir and the leaf name
+        $baseDir = $this->getDirname($this->rootPath);
+        $leafDir = $this->getBasename($this->rootPath);
+
+        // This sets up the cache storage system
+        $filesystemAdapter = new LocalFileAdapter($baseDir);
+        $filesystem = new FlyFilesystem($filesystemAdapter);
+        $this->cachePool = new FilesystemCachePool($filesystem, $leafDir);
+
+        // Here is a dependency to perform additional ops on the cache
+        $this->cacheAdapter = new FilesystemCacheAdapter($filesystem);
+        $this->getCacheAdapter()->setCacheItemPoolInterface($this->getCachePool());
+    }
+
+    public function getCachePool()
+    {
+        if (!$this->cachePool)
+        {
+            throw new InitException("Cache pool not set, have you called init()?");
+        }
+
+        return $this->cachePool;
+    }
+
+    public function getCacheAdapter()
+    {
+        if (!$this->cacheAdapter)
+        {
+            throw new InitException("Cache adapter not set, have you called init()?");
+        }
+
+        return $this->cacheAdapter;
+    }
+
+    /**
+     * Mockable file method
+     *
+     * Could replace this with Service\File
+     *
+     * @param string $path
+     * @return string
+     */
+    protected function getDirname($path)
+    {
+        return dirname($path);
+    }
+
+    /**
+     * Mockable file method
+     *
+     * Could replace this with Service\File
+     *
+     * @param string $path
+     * @return string
+     */
+    protected function getBasename($path)
+    {
+        return basename($path);
+    }
+}
